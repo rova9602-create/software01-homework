5885c3ae5aa1546ee0da94a7e11d74a42e4ed5d8
Moeko Tazoe <rova9602@gmail.com>
2025-09-08 15:08:49 +0300

    testing

---
+++ lecture 2th/Assingment2/task4.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+sum=number1+number2+number3
+product = number1*number2*number3
+average = sum/3
+
+print(f"The sum is {sum:.2f}")
+print(f"The product is {product:.2f}")
+print(f"The average is {average:.2f}")
