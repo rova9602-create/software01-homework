d7d26f883803c55651c76b91a2b8de52260a2e28
Moeko Tazoe <rova9602@gmail.com>
2025-09-08 14:51:49 +0300

    testing

---
+++ lecture 2th/Assingment2/task3.py
@@ -0,0 +1,8 @@
+import math
+
+length=float(input("Enter the length: "))
+wide=float(input("Enter the wide: "))
+perimeter=2*(length+wide)
+area =length*wide
+print(f"The perimeter of the rectangle is {perimeter:.2f}")
+print(f"The area of the rectangle is {area:.2f}")
