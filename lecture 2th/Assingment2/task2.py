1d616e4362ed1b9c25160da7739bf10d22e438b4
Moeko Tazoe <rova9602@gmail.com>
2025-09-08 14:35:35 +0300

    testing

--- lecture 2th/Assingment2/task2.py
+++ lecture 2th/Assingment2/task2.py
@@ -1,2 +1,5 @@
+import math
 radius = float(input("What's radius of the circle?: "))
-print(f"The area of the circle with radius {radius:.2f}")
+area = math.pi*radius**2
+print(f"The area of the circle is {area:.2f}")
+
