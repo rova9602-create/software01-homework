b30e801bca3d09f00336366945980356c38cc613
Moeko Tazoe <rova9602@gmail.com>
2025-11-18 09:48:38 +0200

    testing

--- lecture09_11.11/Assignment_09/Task1.py
+++ lecture09_11.11/Assignment_09/Task1.py
@@ -2,7 +2,7 @@ class Elevator:
     def __init__(self, bottom, top):
         self.bottom = bottom
         self.top = top
-        self.floor = bottom   # 最初は必ず最下階
+        self.floor = bottom
 
     def floor_up(self):
         if self.floor < self.top:
@@ -15,7 +15,11 @@ class Elevator:
             print(f"Elevator is now at floor {self.floor}")
 
     def go_to_floor(self, target):
-        while self.floor < target:
+        while self.floor <= target:
             self.floor_up()
         while self.floor > target:
             self.floor_down()
+
+h = Elevator(1, 10)
+h.go_to_floor(5)
+h.go_to_floor(10)
--- lecture09_11.11/Assignment_09/Task2.py
+++ lecture09_11.11/Assignment_09/Task2.py
@@ -1,3 +1,26 @@
+class Elevator:
+    def __init__(self, bottom, top):
+        self.bottom = bottom
+        self.top = top
+        self.floor = bottom
+
+    def floor_up(self):
+        if self.floor < self.top:
+            self.floor += 1
+            print(f"Elevator is now at floor {self.floor}")
+
+    def floor_down(self):
+        if self.floor > self.bottom:
+            self.floor -= 1
+            print(f"Elevator is now at floor {self.floor}")
+
+    def go_to_floor(self, target):
+        while self.floor < target:
+            self.floor_up()
+        while self.floor > target:
+            self.floor_down()
+
+
 class Building:
     def __init__(self, bottom, top, num_elevators):
         self.elevators = []
@@ -7,3 +30,9 @@ class Building:
     def run_elevator(self, number, destination):
         elevator = self.elevators[number]
         elevator.go_to_floor(destination)
+
+
+b = Building(1, 10, 3)
+
+b.run_elevator(0, 7)
+b.run_elevator(2, 3)
